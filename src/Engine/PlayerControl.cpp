f18c7f710b127c8aed060674b76d6c5d1d499d88
cell <>
2000-09-12 00:46:56 +0000

    separated fCPlayerControl implementation from Player.cpp

---
+++ src/Engine/PlayerControl.cpp
@@ -0,0 +1,335 @@
+/* ===================================================================== *
+ * PlayerControl.cpp (MeV/Engine)
+ * ===================================================================== */
+
+#include "PlayerControl.h"
+
+#include "Destination.h"
+#include "MeVDoc.h"
+#include "PlaybackTask.h"
+#include "Player.h"
+
+// ---------------------------------------------------------------------------
+// Operations
+
+void
+CPlayerControl::DoAudioFeedback(
+	CMeVDoc *doc,
+	enum E_EventAttribute feedbackAttribute,
+	uint8 attributeValue,
+	const Event *demoEvent)
+{
+	Event fbEvents[3];
+	Event *modEvent = &fbEvents[0];
+	Event *noteStart = &fbEvents[1];
+	Event *noteStop = &fbEvents[2];
+
+	if (doc && demoEvent)
+	{
+		int32 channel = (feedbackAttribute == EvAttr_Channel)
+						? attributeValue
+						: demoEvent->GetVChannel();
+		Destination	*dest = doc->GetVChannel(channel);
+		modEvent->stack.actualPort = dest->m_producer;	
+		modEvent->stack.actualChannel = dest->channel;
+	}
+	else
+	{
+		int32 channel = (feedbackAttribute == EvAttr_Channel)
+						? attributeValue
+						: 0;
+		modEvent->stack.actualPort = NULL;
+		modEvent->stack.actualChannel = channel;
+	}
+
+	// Set up a default list of events which plays an average note.
+	modEvent->stack.start = 0;
+	modEvent->stack.task = MAX_FEEDBACK_TASKS;
+	modEvent->note.command = 0;
+
+	*noteStart = *modEvent;
+	*noteStop = *modEvent;
+
+	noteStart->stack.start = gPrefs.feedbackDelay;
+	noteStop->stack.start = gPrefs.feedbackDelay + 450;
+	noteStart->note.command = EvtType_Note;
+	noteStop->note.command = EvtType_NoteOff;
+	if (demoEvent && (demoEvent->Command() == EvtType_Note))
+	{
+		noteStart->note.pitch = noteStop->note.pitch = demoEvent->note.pitch;
+		noteStart->note.attackVelocity = demoEvent->note.attackVelocity;
+		noteStop->note.releaseVelocity = demoEvent->note.releaseVelocity;
+	}
+	else
+	{
+		noteStart->note.pitch = noteStop->note.pitch = 60;
+		noteStart->note.attackVelocity = noteStop->note.releaseVelocity = 64;
+	}
+
+	switch (feedbackAttribute)
+	{
+		case EvAttr_Channel:
+		{
+		 	noteStart->stack.actualChannel = noteStop->stack.actualChannel = attributeValue;
+			break;
+		}
+		case EvAttr_Pitch:
+		{
+			noteStart->note.pitch = noteStop->note.pitch = attributeValue;
+			break;
+		}	
+		case EvAttr_AttackVelocity:
+		{
+			noteStart->note.attackVelocity = attributeValue;
+			break;
+		}
+		case EvAttr_ReleaseVelocity:
+		{
+			noteStart->note.releaseVelocity = attributeValue;
+			break;
+		}
+		case EvAttr_Program:
+		{
+			modEvent->programChange.command = EvtType_ProgramChange;
+			modEvent->programChange.program = attributeValue;
+			modEvent->programChange.bankMSB = MIDIValueUnset;
+			break;
+		}
+		default:
+		{
+			return;
+		}
+	}
+
+	StPlayerLock lock;
+
+	// Kill the previous feedback event
+	thePlayer.wildGroup->FlushEvents();
+
+	// If it's a note event, play the note.
+	// If the doc is already playing, play only the feedback event, no note,
+	// else play the feedback event and a note to hear it.
+	if (modEvent->note.command == 0)
+		thePlayer.QueueImmediate(&fbEvents[1], 2);
+	else if (IsPlaying(doc))
+		thePlayer.QueueImmediate(&fbEvents[0], 1);
+	else
+		thePlayer.QueueImmediate(&fbEvents[0], 3);
+}
+
+void
+CPlayerControl::InitPlayer()
+{
+	thePlayer.Initialize();
+}
+
+void
+CPlayerControl::PlaySong(
+	CMeVDoc *document,
+	int32 trackID,
+	long locTime,
+	enum ELocateTarget locateTarget,
+	int32 duration,
+	enum ESyncType syncType,
+	int16 options)
+{
+	// Make sure document doesn't go away!
+	document->Acquire();
+	
+	CommandArgs args;
+	args.document = document;
+	args.trackID = trackID;
+	args.locTime = locTime;
+	args.locateTarget = locateTarget;
+	args.duration = duration;
+	args.syncType = syncType;
+	args.options = options;
+	write_port(thePlayer.Port(), Command_Start, &args, sizeof(args));
+}
+
+void
+CPlayerControl::StopSong(
+	CMeVDoc *document)
+{
+	// Make sure document doesn't go away!
+	document->Acquire();
+
+	CommandArgs args;
+	args.document = document;
+	write_port(thePlayer.m_port, Command_Stop, &args, sizeof(args));
+}
+
+bool
+CPlayerControl::IsPlaying(
+	CMeVDoc *document)
+{
+	StPlayerLock lock;
+
+	CPlaybackTaskGroup *group = thePlayer.FindGroup(document);
+	if (group
+	 && ((group->flags & CPlaybackTaskGroup::Clock_Stopped) == false))
+		return true;
+
+	return false;
+}
+
+bool
+CPlayerControl::GetPlaybackState(
+	CMeVDoc *document,
+	PlaybackState &outState)
+{
+	StPlayerLock lock;
+
+	CPlaybackTaskGroup *group = thePlayer.FindGroup(document);
+	if (group)
+	{
+		outState.running = !(group->flags & CPlaybackTaskGroup::Clock_Stopped);
+		outState.realTime = group->real.time;
+		outState.meteredTime = group->metered.time;
+		return true;
+	}
+
+	return false;
+}
+
+bool
+CPlayerControl::PauseState(
+	CMeVDoc *document)
+{
+	StPlayerLock lock;
+
+	CPlaybackTaskGroup *group = thePlayer.FindGroup(document);
+	if (group
+	 && ((group->flags & CPlaybackTaskGroup::Clock_Paused) != false))
+		return true;
+
+	return false;
+}
+
+void
+CPlayerControl::SetPauseState(
+	CMeVDoc *document,
+	bool state)
+{
+	StPlayerLock lock;
+
+	CPlaybackTaskGroup *group = thePlayer.FindGroup(document);
+	if (group)
+	{
+		// REM: If externally sync'd, then no effect.
+		if (state)
+			group->flags |= CPlaybackTaskGroup::Clock_Paused;
+		else
+			group->flags &= ~CPlaybackTaskGroup::Clock_Paused;
+	}
+}
+
+void
+CPlayerControl::TogglePauseState(
+	CMeVDoc *document)
+{
+	StPlayerLock lock;
+
+	CPlaybackTaskGroup *group = thePlayer.FindGroup(document);
+	if (group)
+	{
+		// REM: If externally sync'd, then no effect.
+		group->flags ^= CPlaybackTaskGroup::Clock_Paused;
+	}
+}
+
+double
+CPlayerControl::Tempo(
+	CMeVDoc *document)
+{
+	StPlayerLock lock;
+
+	CPlaybackTaskGroup *group = thePlayer.FindGroup(document);
+
+	if (group)
+		return PeriodToRate(group->CurrentTempoPeriod());
+	if (document)
+		return document->InitialTempo();
+
+	return CMeVDoc::DEFAULT_TEMPO;
+}
+	
+void
+CPlayerControl::SetTempo(
+	CMeVDoc *document,
+	double tempo)
+{
+	long period = RateToPeriod(tempo);
+
+	StPlayerLock lock;
+
+	CPlaybackTaskGroup *group = thePlayer.FindGroup(document);
+	if (group)
+		group->ChangeTempo(period, group->metered.time, 0, ClockType_Metered);
+}
+
+char *
+CPlayerControl::PortName(
+	uint32 index)
+{
+	//maybe we should be returning the name of the producer on this index.
+	if ((index < 0) || (index >= Max_MidiPorts))
+		return NULL;
+	return thePlayer.m_portInfo[index].portName;
+}
+	
+void
+CPlayerControl::SetPortName(
+	uint32 index,
+	char *name)
+{
+	//maybe we should be setting the name of the producer on this index.
+	CPlayer::PortInfo *pi;
+	if ((index < 0) || (index >= Max_MidiPorts))
+		return;
+	pi = &thePlayer.m_portInfo[index];
+	strncpy(pi->portName, name, sizeof(pi->portName));
+	pi->portName[sizeof(pi->portName) - 1] = '\0';
+}
+	
+int32
+CPlayerControl::GetPlaybackMarkerTimes(
+	CTrack *track,
+	int32 *resultBuf,
+	int32 bufSize)
+{
+	int32 count = 0;
+	CPlaybackTaskGroup *group;
+	CPlaybackTask *th;
+
+	// Lock list of contexts while we are looking through it
+	StPlayerLock lock;
+
+	// Search each context
+	for (group = (CPlaybackTaskGroup *)thePlayer.m_groupList.First();
+		 group != NULL && count < bufSize;
+		 group = (CPlaybackTaskGroup *)group->Next())
+	{
+		if (!(group->flags &
+			 (CPlaybackTaskGroup::Clock_Stopped
+			| CPlaybackTaskGroup::Clock_AwaitingSync
+			| CPlaybackTaskGroup::Clock_Locating)))
+		{
+			for (th = (CPlaybackTask *)group->tasks.First();
+				 th != NULL;
+				 th = (CPlaybackTask *)th->Next())
+			{
+				if (th->track == track)
+				{
+					resultBuf[count++] = th->CurrentTime();
+					if (count >= bufSize)
+						break;
+				}
+			}
+		}
+	}
+
+	return count;
+}
+
+// END - PlayerControl.cpp
--- src/Engine/PlayerControl.h
+++ src/Engine/PlayerControl.h
@@ -40,6 +40,7 @@
 #ifndef __C_PlayerControl_H__
 #define __C_PlayerControl_H__
 
+#include "Event.h"
 #include "MeV.h"
 
 class CMeVDoc;
@@ -48,32 +49,36 @@ class CTrack;
 // ---------------------------------------------------------------------------
 // Commands sent to player
 
-enum EMidiCommands {
-	Command_Start	= 0,		// Args = document,track,time,locTarget,SyncType,options
+enum EMidiCommands
+{
+	Command_Start	= 0,	// Args = document,track,time,locTarget,SyncType,options
 	Command_Stop,			// Args = document
 	Command_Pause,			// Args = document
 	Command_Continue,		// Args = document
 	Command_Quit,			// Args = <none>
-	Command_Attention,		// Args = <none>
+	Command_Attention		// Args = <none>
 };
 
 // ---------------------------------------------------------------------------
 // Enumeration values used as parameters when launching a sequence
 
-enum ELocateTarget {
+enum ELocateTarget
+{
 	LocateTarget_Continue = 0,		// no locate, just play from pos
 	LocateTarget_Real,				// locate to real pos
-	LocateTarget_Metered,				// locate to metered pos
+	LocateTarget_Metered			// locate to metered pos
 };
 
-enum ESyncType {
-	SyncType_FreeRunning = 0,			// not synced with anything
+enum ESyncType
+{
+	SyncType_FreeRunning = 0,		// not synced with anything
 	SyncType_SongInternal,			// sync with internal song time
 	SyncType_SongMTC,				// sync song with MIDI Time Code
-	SyncType_SongMidiClocks,			// sync song with MIDI clocks
+	SyncType_SongMidiClocks			// sync song with MIDI clocks
 };
 
-enum EPlaybackOption {
+enum EPlaybackOption
+{
 	PB_Paused		= (1<<0),		// set up track initially paused
 	PB_Record		= (1<<1),		// record while playing
 	PB_Loop			= (1<<2),		// loop over and over
@@ -85,105 +90,136 @@ enum EPlaybackOption {
 
 class CMeVDoc;
 
-struct CommandArgs {
-	CMeVDoc			*document;			// Pointer to document (validate)
-	int32			trackID;				// ID of track to play
-	long				locTime;				// time to play at
-	int8				locateTarget;		// Locator target type
-	long				duration;			// Duration of playback
-	int8				syncType;			// Synchronization type
-	int16			options;				// Various options flags
+struct CommandArgs
+{
+	CMeVDoc	*	document;			// Pointer to document (validate)
+	int32		trackID;			// ID of track to play
+	long		locTime;			// time to play at
+	int8		locateTarget;		// Locator target type
+	long		duration;			// Duration of playback
+	int8		syncType;			// Synchronization type
+	int16		options;			// Various options flags
 };
 
 // ---------------------------------------------------------------------------
 // State of player...
 
-struct PlaybackState {
-	bool				running;
-	long				realTime;
-	long				meteredTime;
+struct PlaybackState
+{
+	bool		running;
+	long		realTime;
+	long		meteredTime;
 };
 
 // ---------------------------------------------------------------------------
 // PlayerControl utility class
 
-class CPlayerControl {
-public:
-	static void InitPlayer();
+class CPlayerControl
+{
 
-		/**	Play some audible feedback. */
-	static void DoAudioFeedback(
-		CMeVDoc					*doc,				// doc (for VChannel)
-		enum E_EventAttribute		feedbackAttribute,	// Which attr is changing
-		uint8					attributeValue,		// New attribute value
-		const Event				*demoEvent );			// Prototype event
+public:							// Operations
+
+	/**	Audio feedback routine for user interface.
+		@param doc					For Destination
+		@param feedbackAttribute	Which attribute is changing.
+		@param attributeValue		New attribute value
+		@param demoEvent			Prototype event
+	*/
+	static void					DoAudioFeedback(
+									CMeVDoc	*doc,
+									enum E_EventAttribute feedbackAttribute,
+									uint8 attributeValue,
+									const Event *demoEvent);
 		
-		/**	Start playing a song. */
-	static void PlaySong(
-		CMeVDoc				*document,
-		int32				trackID,
-		long					locTime,
-		enum ELocateTarget	locateTarget,
-		long					duration,
-		enum ESyncType		syncType,
-		int16				options );
+	static void					InitPlayer();
+
+	/**	Determine if this document or any tracks in it are playing. */
+	static bool					IsPlaying(
+									CMeVDoc *document);
+	/**	Start playing a song. */
+	static void					PlaySong(
+									CMeVDoc *document,
+									int32 trackID,
+									long locTime,
+									enum ELocateTarget locateTarget,
+									long duration,
+									enum ESyncType syncType,
+									int16 options);
+
+	/**	Stop playing a song. */
+	static void					StopSong(
+									CMeVDoc *document);
+	
+
+	/**	If this document is playing (or paused), return the current playback state */
+	static bool					GetPlaybackState(
+									CMeVDoc *document,
+									PlaybackState &outState);
+
+	/**	Determine the pause state for this document. */
+	static bool					PauseState(
+									CMeVDoc *document);
+	/** Set the state of the pause flag for this document, if it's playing. */
+	static void					SetPauseState(
+									CMeVDoc *document,
+									bool state);
+	/** Toggle the state of the pause flag for this document, if it's playing. */
+	static void					TogglePauseState(
+									CMeVDoc *document);
+
+	/**	Given a track, find all of the playback threads that are currently
+		playing that track, and return the playback time of each thread
+		relative to the start of the track. This is used to provide visual
+		feedback of the playback.
+		@param track	Track to search for.
+		@param buffer	Buffer to place results in.
+		@param size 	Sizeof buffer.
+	*/
+	static int32				GetPlaybackMarkerTimes(
+									CTrack *track,
+									int32 *buffer,
+									int32 size);
 		
-		/**	Determine if this document or any tracks in it are playing. */
-	static bool IsPlaying( CMeVDoc *document );
-	
-		/**	If this document is playing (or paused), return the current playback state */
-	static bool GetPlaybackState( CMeVDoc *document, PlaybackState &outState );
+	/**	Return the current tempo. */
+	static double				Tempo(
+									CMeVDoc *document);
+	/**	Set the current tempo */
+	static void					SetTempo(
+									CMeVDoc *document,
+									double tempo);
 
-		/**	Determine the pause state for this document. */
-	static bool PauseState( CMeVDoc *document );
+	/**	Get the port name of the Nth port. */
+	static char *				PortName(
+									uint32 index);
+	/**	Set the port name of the Nth port. */
+	static void					SetPortName(
+									uint32 index,
+									char *name);
 
-		/** Set the state of the pause flag for this document, if it's playing. */
-	static void SetPauseState( CMeVDoc *document, bool inPauseState );
+	/**	Get the port device string of the Nth port. */
+	static const char *			PortDevice(
+									uint32 index);
+	/**	Set the port device string of the Nth port. */
+	static bool					SetPortDevice(
+									uint32 index,
+									char *name);
 
-		/** Toggle the state of the pause flag for this document, if it's playing. */
-	static void TogglePauseState( CMeVDoc *document );
+	/** In general we don't seem to like working with pointers 
+		here, but for now...
+	*/
+	static bool					SetPortConnect(
+									uint32 index,
+									BMidiConsumer *sink);
 
-		/**	Stop playing a song. */
-	static void StopSong( CMeVDoc *document );
-	
-		/**	Return the current tempo. */
-	static double Tempo( CMeVDoc *document );
-	
-		/**	Set the current tempo */
-	static void SetTempo( CMeVDoc *document, double newTempo );
-
-		/**	Given a track, find all of the playback threads that are currently
-			playing that track, and return the playback time of each thread
-			relative to the start of the track. This is used to provide visual
-			feedback of the playback.
-		*/
-	static int32 GetPlaybackMarkerTimes(
-		CTrack		*track,					// Track to search for
-		int32		*resultBuf,				// Buffer to place results in
-		int32		bufSize );				// size of buffer
-		
-		/**	Get the port name of the Nth port. */
-	static char *PortName(uint32 inPortIndex );
-	
-		/**	Set the port name of the Nth port. */
-	static void SetPortName(uint32 inPortIndex, char *inPortName );
-	
-		/**	Get the port device string of the Nth port. */
-	
-	static const char *PortDevice(uint32 inPortIndex );
-		/**	Set the port device string of the Nth port. */
-	static bool SetPortDevice(uint32 inPortIndex, char *inPortName );
-	//in general we don't seem to like working with pointers here, but for now...
-	static bool SetPortConnect(uint32 inPortIndex, BMidiConsumer *sink );
-	
 	//returns the midiroster proviced endpoint ID.
-	static int32 PortDeviceID(uint32 inPortIndex);
-	static bool DeleteDevice(int inPortIndex);
+	static int32				PortDeviceID(
+									uint32 index);
+	static bool					DeleteDevice(
+									int index);
 	
-	static int CountDefinedPorts();
-	static bool IsDefined(uint32 inPortIndex);
-
-private:
+	static int					CountDefinedPorts();
+	static bool					IsDefined(
+									uint32 index);
 };
 
 #endif /* __C_PlayerControl_H__ */
