f67090fda30c6e5a25d26b193b072fa0be03ae7c
malouin <>
2000-09-14 03:06:03 +0000

    throws exception on error.

--- src/Support/BeFileReader.cpp
+++ src/Support/BeFileReader.cpp
@@ -6,6 +6,7 @@
  * ===================================================================== */
  
 #include "BeFileReader.h"
+#include "Error.h"
 
 	/**	Main reading function. This is pure virtual, and must be
 		implemented by subclasses.
@@ -37,6 +38,16 @@ int32 CBeFileReader::Read( void *buffer, int32 inLength, bool inPartialOK )
 	/**	Read exactly 'inLength' bytes, or toss an exception. */
 void CBeFileReader::MustRead( void *buffer, int32 inLength )
 {
-	int32 result = file.Read( buffer, inLength );
-	CheckBeError( result );
+	class EndOfFile :
+		public IError
+	{
+	public:
+		virtual	const char*		Description() const
+									{ return "File is incomplete or corrupt"; }
+	};
+
+	int32 bytesRead = file.Read( buffer, inLength );
+	CheckBeError( bytesRead );
+	if (bytesRead < inLength)
+		throw EndOfFile();
 }
