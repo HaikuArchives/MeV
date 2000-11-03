400afdcabe4ab4a8ab0459b7514e5be898c6f6eb
cell <>
2000-11-03 08:57:22 +0000

    added standard be transport button bitmaps

---
+++ resources/Buttons/add_bitmaps.sh
@@ -0,0 +1,32 @@
+# add_bitmaps.sh
+
+xres -o ../Buttons.rsrc \
+-a RAWT:600:PlayPause "600_PlayPause.tga" \
+-a RAWT:601:PlayPause_Disabled "601_PlayPause_Disabled.tga" \
+-a RAWT:602:PlayPause_Pressed "602_PlayPause_Pressed.tga" \
+-a RAWT:603:PlayPause_Playing "603_PlayPause_Playing.tga" \
+-a RAWT:604:PlayPause_Paused "604_PlayPause_Paused.tga" \
+-a RAWT:610:Stop "610_Stop.tga" \
+-a RAWT:611:Stop_Disabled "611_Stop_Disabled.tga" \
+-a RAWT:612:Stop_Pressed "612_Stop_Pressed.tga" \
+-a RAWT:620:Record "620_Record.tga" \
+-a RAWT:621:Record_Disabled "621_Record_Disabled.tga" \
+-a RAWT:622:Record_Pressed "622_Record_Pressed.tga" \
+-a RAWT:623:Record_Recording "623_Record_Recording.tga" \
+-a RAWT:630:SkipBack "630_SkipBack.tga" \
+-a RAWT:631:SkipBack_Disabled "631_SkipBack_Disabled.tga" \
+-a RAWT:632:SkipBack_Pressed "632_SkipBack_Pressed.tga" \
+-a RAWT:640:Rewind "640_Rewind.tga" \
+-a RAWT:641:Rewind_Disabled "641_Rewind_Disabled.tga" \
+-a RAWT:642:Rewind_Pressed "642_Rewind_Pressed.tga" \
+-a RAWT:650:FastForward "650_FastForward.tga" \
+-a RAWT:651:FastForward_Disabled "651_FastForward_Disabled.tga" \
+-a RAWT:652:FastForward_Pressed "652_FastForward_Pressed.tga" \
+-a RAWT:660:SkipForward "660_SkipForward.tga" \
+-a RAWT:661:SkipForward_Disabled "661_SkipForward_Disabled.tga" \
+-a RAWT:662:SkipForward_Pressed "662_SkipForward_Pressed.tga" \
+-a RAWT:670:LoopOff "670_LoopOff.tga" \
+-a RAWT:671:LoopOff_Disabled "671_LoopOff_Disabled.tga" \
+-a RAWT:672:LoopOff_Pressed "672_LoopOff_Pressed.tga" \
+-a RAWT:673:LoopOn "673_LoopOn.tga" \
+-a RAWT:674:LoopOn_Pressed "674_LoopOn_Pressed.tga"
